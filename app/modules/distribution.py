81299b28d839c8676577286100145b947d344eb7
Tomich37 <Tomich7372@gmail.com>
2023-06-19 19:30:16 +0700

    Небольшие изменения

--- app/modules/distribution.py
+++ app/modules/distribution.py
@@ -15,21 +15,25 @@ class Distribution:
         # Получение текста
         text = message.text.split(' ', 1)[1]
         user_id = message.from_user.id
+        user_role = self.database.get_user_role(user_id)
 
         # Сохранение текста в БД и получение id
         distribution_id = self.database.save_distribution_text(text)
-
-        if distribution_id is not None:
-            # Создание кнопок "Отрпавить" и "отмена"
-            keyboard = types.InlineKeyboardMarkup()
-            send_button = types.InlineKeyboardButton(text="Отправить", callback_data=f"send_distribution_{distribution_id}")
-            cancel_button = types.InlineKeyboardButton(text="Отменить", callback_data="cancel_distribution")
-            keyboard.add(send_button, cancel_button)
-
-            # Отправка сообщения для подтверждения отправки
-            self.bot.send_message(user_id, f"Сообщение для рассылки:\n\n{text}", reply_markup=keyboard)
+        if user_role == 'user':
+            markup = self.moderation.user_markup()
+            self.bot.send_message(user_id, "У вас недостаточно прав.", reply_markup=markup)
         else:
-            self.bot.send_message(user_id, "Не удалось создать рассылку.")
+            if distribution_id is not None:
+                # Создание кнопок "Отрпавить" и "отмена"
+                keyboard = types.InlineKeyboardMarkup()
+                send_button = types.InlineKeyboardButton(text="Отправить", callback_data=f"send_distribution_{distribution_id}")
+                cancel_button = types.InlineKeyboardButton(text="Отменить", callback_data="cancel_distribution")
+                keyboard.add(send_button, cancel_button)
+
+                # Отправка сообщения для подтверждения отправки
+                self.bot.send_message(user_id, f"Сообщение для рассылки:\n\n{text}", reply_markup=keyboard)
+            else:
+                self.bot.send_message(user_id, "Не удалось создать рассылку.")
 
     # Обработка кнопки "Создать рассылку"
     def distribution_button_click(self, user_id):
